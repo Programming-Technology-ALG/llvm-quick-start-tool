9aa9afc02d07e50403ccfa4a09c95342dead5b8a
Trutnev Aleksei <alexgiving@mail.ru>
2021-10-13 17:35:13 +0300

    Use WORKDIR

--- llvm_quick_start.sh
+++ llvm_quick_start.sh
@@ -5,52 +5,53 @@
 # Year: 2021
 
 if [ -z $* ]; then
-	echo "No options found!"
-	echo "Use -h to help"
-	exit 1
+    echo "No options found!"
+    echo "Use -h to help"
+    exit 1
 fi
 
 echo "Quick-start tool for llvm project is started..."
+cd ../llvm-project/
+WORKDIR=$(PWD)
+BUILD_DIR=${WORKDIR}/build
+echo "WORKDIR is changed to" ${WORKDIR}
 
 while getopts "ibth" opt; do
   case "$opt" in
-  	h) # Help
-		echo "Help:"
-		echo "-i install all required packages"
-		echo "-b build llvm project to {llvm_folder}/build"
-		echo "-t test llvm project from {llvm_folder}/build directory"
-		echo "To use -bt options you must be in {llvm_folder} directory"
-		;;
+    h) # Help
+        echo "Help:"
+        echo "-i install all required packages"
+        echo "-b build llvm project to " ${BUILD_DIR}
+        echo "-t test llvm project from " ${BUILD_DIR}
+        ;;
     i) # Install packages
-    	echo "Get brew package system"
-		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        echo "Get brew package system"
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-		echo "Install ninja package"
-		brew install ninja
+        echo "Install ninja package"
+        brew install ninja
 
-		echo "Install cmake package"
-		brew install cmake
+        echo "Install cmake package"
+        brew install cmake
 
-		echo "Install llvm clang package"
-		brew install llvm
-		;;
+        echo "Install llvm clang package"
+        brew install llvm
+        ;;
     b) # Build
-		echo "Prepare a build"
-		mkdir build
-		cd build
-		cmake -G "Ninja" -DLLVM_ENABLE_PROJECTS=clang -DLLVM_BUILD_EXAMPLES=ON -DLLVM_TARGETS_TO_BUILD="host" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_ENABLE_RTTI=ON ../llvm
-      	;;
+        echo "Prepare a build"
+        mkdir build
+        cd ${BUILD_DIR}
+        cmake -G "Ninja" -DLLVM_ENABLE_PROJECTS=clang -DLLVM_BUILD_EXAMPLES=ON -DLLVM_TARGETS_TO_BUILD="host" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_ENABLE_RTTI=ON ../llvm
+        ;;
     t) # Run ninja tests
-		echo "Run ninja test"
-		WORKDIR=$(PWD) 
-		BUILD_DIR=${WORKDIR}/build
-		if test -d "$BUILD_DIR"; then
-			cd $BUILD_DIR
-		fi
-    		ninja
-      	;;
+        echo "Run ninja test"
+        if test -d "$BUILD_DIR"; then
+            cd $BUILD_DIR
+        fi
+            ninja
+        ;;
     \?)
-	    echo "Invalid option: -$OPTARG" >&4
-	    ;;
+        echo "Invalid option: -$OPTARG" >&4
+        ;;
   esac
 done
